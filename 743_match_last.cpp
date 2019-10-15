cd28ba48d2b47e8949e44f2459316a12487d396e
FakeBeetle <47056213+FakeBeetle@users.noreply.github.com>
2019-10-15 17:10:41 +0800

    Update 743_match_last.cpp

--- 743_match_last.cpp
+++ 743_match_last.cpp
@@ -12,46 +12,42 @@ using std::string;
 using std::vector;
 using std::pair;
 using std::getline;
-
-stack<pair<char, int>> reverse(stack<pair<char, int>> old_stack) {
-	stack<pair<char, int>> new_stack;
-	while (!old_stack.empty()) {
-		new_stack.push(old_stack.top());
-		old_stack.pop();
-	}
-	return new_stack;
+/*
+bool char_in_str(string s,char a) {
+	for (int i = 0; i != s.length(); i++)
+		if (a == s[i])
+			return true;
+	return false;
 }
-
+bool flag(string s) {
+	return char_in_str(s, '(') || char_in_str(s, ')') || char_in_str(s, '[') || char_in_str(s, ']') || char_in_str(s, '{') || char_in_str(s, '}');
+}
+*/
 int find_unmatched(string s) {
+	
 	stack<pair<char, int>> visited_brackets;
-	if (s.empty()) {
-		return SUCCESS;
-	}
 	for (int i = 0; i != s.length(); ++i) {
 		if (s.at(i) == '(' || s.at(i) == '[' || s.at(i) == '{') {
-			visited_brackets.push(pair<char, int>(s.at(i), i));
+			visited_brackets.push(pair<char,int>(s.at(i),i));
 		}
 		else if (s.at(i) == ')' || s.at(i) == ']' || s.at(i) == '}') {
 			char paired_bracket = s.at(i) - 1 - (s.at(i) != ')');
-			pair<char, int> current('\0', i);
-			stack<pair<char, int>> temp;
-			stack<pair<char, int>> temp_reverse_stack = reverse(visited_brackets);
-			while (!temp_reverse_stack.empty() && current.first != paired_bracket) {
-				current = temp_reverse_stack.top();
-				temp_reverse_stack.pop();
+			pair<char,int> current('\0',i);
+			stack<pair<char,int>> temp;
+			while (!visited_brackets.empty() && current.first != paired_bracket) {
+				current = visited_brackets.top();
+				visited_brackets.pop();
 				temp.push(current);
 			}
 			if (current.first == paired_bracket) {
 				temp.pop();//首个元素是配对括号
 				while (!temp.empty()) {
-					temp_reverse_stack.push(temp.top());
+					visited_brackets.push(temp.top());
 					temp.pop();
 				}
-				visited_brackets = reverse(temp_reverse_stack);
 			}
-
 			else
-				return i + 1;
+				return i+1;
 		}
 
 	}
@@ -59,12 +55,12 @@ int find_unmatched(string s) {
 		return SUCCESS;//
 	}
 	else {
-		pair<char, int> c;
+		pair<char,int> c;
 		while (!visited_brackets.empty()) {
 			c = visited_brackets.top();
 			visited_brackets.pop();
 		}
-		return c.second + 1;
+		return c.second+1;
 	}
 }
 int main() {
@@ -72,9 +68,11 @@ int main() {
 	while (getline(cin, text)) {
 		int x = find_unmatched(text);
 		if (x)
-			cout << x << std::endl;
-		else
-			cout << "Success" << std::endl;
+			cout << x << "\n";
+		else {
+			//if (flag(text))
+				cout << "Success\n";
+		}
 	}
 	return 0;
 }
